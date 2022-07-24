7dffeeeb8bc32f74480bfcb1fdfab779db27939a
Pedro Cabra <jando2797@gmail.com>
2022-07-24 11:37:39 -0500

    se agrega funcion para el ipc

--- scrapping_spanish.R
+++ scrapping_spanish.R
@@ -10,6 +10,7 @@
 ######################################
 packages_download <- function(){
   if (!require(openxlsx))install.packages("openxlsx");library(openxlsx)
+  if (!require(httr))install.packages("httr");library(httr)
 }             
 
 
@@ -45,4 +46,84 @@ get.TRM <- function(){
   print("Fuente: Superintendencia Financiera Colombiana")
   #se retorna
   return(TRM)
+}
+
+######################################
+# IPC: Banco de la republica de Colombia
+######################################
+get.IPC = function(){
+  
+  #' Funcion que hace webscrapping a los exceles del Banco de la republica de Colombia
+  #' para obtener el historico del IPC en niveles mensuales
+  #'
+  #' OUTPUT:
+  #' @return data frame con las fechas y el IPC
+  
+  #link del excel del ipc del banrep
+  link <- "https://totoro.banrep.gov.co/analytics/saw.dll?Download&Format=excel2007&Extension=.xls&BypassCache=true&lang=es&NQUser=publico&NQPassword=publico123&path=%2Fshared%2FSeries%20Estad%C3%ADsticas_T%2F1.%20IPC%20base%202018%2F1.2.%20Por%20a%C3%B1o%2F1.2.5.IPC_Serie_variaciones"
+  
+  #se hace un print de que inicio el proceso
+  print("Extrayendo datos, puede tomar unos minutos")
+  #se crea un archov temporal
+  path_excel <- tempfile(fileext = ".xlsx")
+  
+  #se extraen los datos de la descarga
+  
+  while(class(try(read.xlsx(path_excel, sheet = 1, detectDates = F),silent=T))=="try-error"){
+    r <- GET(link,
+            add_headers(
+              Host="totoro.banrep.gov.co",
+              `User-Agent`="Mozilla/5.0 (Windows NT 6.3; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0",
+              Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+              `Accept-Language` = "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
+              `Accept-Encoding` = "gzip, deflate",
+              Connection = "keep-alive"
+            ))
+    #se pasan a formato excel
+    bin <- content(r, "raw")
+    writeBin(bin, path_excel)
+    
+    #se leen
+    d <- try(read.xlsx(path_excel, sheet = 1, detectDates = F),silent=T)
+  }
+  
+  #eliminando el temporal
+  unlink(path_excel)
+  #se arregla el formato
+  ##se dejan solo las fechas y el ipc (primeras dos columnas)
+  d <- d[,1:2]
+  ##se pasa la primera columna a numerico
+  d[,1] <- suppressWarnings(as.numeric(d[,1]))
+  ##se deja de una vez solo lo que no sea NA en ninguna de las dos columnas
+  ##asi se borra tanto lo que era texto antes como lo que no tiene dato
+  d <- d[complete.cases(d),]
+  ##las fechas estan en numerico pero es un formato anho mes, se ponen todas al final
+  ##del mes
+  ###se extrae el anho (los primeros 4 numeros)
+  ano <- as.numeric(substr(d[,1],start=1,stop=4))
+  ####se extrae el mes
+  mes <- as.numeric(substr(d[,1],start=5,stop=nchar(d[,1])))
+  ###se suma uno al mes, para luego restarle uno a la fecha (porque no se en que dia acaba cada mes)
+  mes <- mes +1
+  ano <- ifelse(mes==13,ano+1,ano)
+  mes <- ifelse(mes==13,1,mes)
+  ###se construye la fecha
+  fechasIPC <- as.Date(paste(ano,mes,1,sep="/"),format="%Y/%m/%d")-1
+  
+  #se arreglan los nombres de las columnas
+  names(d) <- c("Fecha", "IPC")
+  #se intercambia la columna de fecha por las fechas construidas
+  d$Fecha <- fechasIPC
+  #se ordena
+  d <- d[order(d$Fecha),]
+  #se quitan los nombres de las filas
+  rownames(d) <- NULL
+  #se vuelven numeros las tasas
+  d[,2] <- as.numeric(d[,2])
+  #se anuncia cuantos datos se consiguieron
+  print(paste0("Se obtuvo datos para el IPC desde ", d$Fecha[1], " hasta ", d$Fecha[nrow(d)] ))
+  print("Fuente: Banco de la Republica de Colombia")
+  #se returna el data frame
+  return(d)
+  
 }
