6e4eda0cc80184dd4056f2f1fc41d581946d994f
Pedro Cabra <jando2797@gmail.com>
2022-07-25 16:06:05 -0500

    se agrega script de ejemplos

---
+++ example_scrapping_macro.R
@@ -0,0 +1,8 @@
+
+# Setting the path 
+# Definiendo la ruta
+path <- gsub("example_scrapping_macro.R","",rstudioapi::getActiveDocumentContext()$path)
+
+# Loading the required packages y loading the functions
+# Cargando paquetes y script de funciones
+
